4d1e472d4eaeec20dec1c111b001d071f90dc0e9
Alex <al.gr.0017@gmail.com>
2023-03-22 17:24:08 +0300

    feat: add header, bootstrap, material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { OrganizationModule } from './organization/organization.module';
+import { TaskModule } from './task/task.module';
+import { UserModule } from './user/user.module';
+import { CoreModule } from './core/core.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule,
+    OrganizationModule,
+    TaskModule,
+    UserModule,
+    CoreModule,
+    NgbModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/task/task.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TaskComponent } from './task/task.component';
+
+@NgModule({
+  declarations: [
+    TaskComponent
+  ],
+  imports: [CommonModule],
+})
+export class TaskModule {}
